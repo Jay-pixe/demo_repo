c0c60b7be2f33623ae3781cb585991eb34cce1d4
Jay-pixe <75147846+Jay-pixe@users.noreply.github.com>
2023-11-16 02:21:05 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 // Add this JavaScript for responsive navigation
 document.addEventListener('DOMContentLoaded', function () {
     var menuToggle = document.getElementById('menu-toggle');
+    var closeBtn = document.getElementById('close-btn');
     var nav = document.querySelector('nav');
 
     menuToggle.addEventListener('click', function () {
+        toggleNav();
+    });
+
+    closeBtn.addEventListener('click', function () {
+        toggleNav();
+    });
+
+    function toggleNav() {
         if (window.getComputedStyle(nav).display === 'none') {
             nav.style.display = 'flex';
+            menuToggle.style.display = 'none';
+            closeBtn.style.display = 'block';
         } else {
             nav.style.display = 'none';
+            menuToggle.style.display = 'block';
+            closeBtn.style.display = 'none';
         }
-    });
+    }
 });
