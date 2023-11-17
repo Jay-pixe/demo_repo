38331980cf20b772ee5f8e840dcc6bb634246866
Jay-pixe <75147846+Jay-pixe@users.noreply.github.com>
2023-11-17 02:22:13 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,29 @@ function toggleMenu() {
     nav.classList.toggle('open');
 }
 
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    var slogan = document.querySelector(".slogan");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+
+    setTimeout(showSlides, 10000); // Change slide every 5 seconds
+}
+
+function changeSlide(n) {
+    showSlides(slideIndex += n);
+}
