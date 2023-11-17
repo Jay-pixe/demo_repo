8667d9f6acc73aa0fbea1ee2bab3ed050935a579
Jay-pixe <75147846+Jay-pixe@users.noreply.github.com>
2023-11-17 01:51:21 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,25 +3,3 @@ function toggleMenu() {
     nav.classList.toggle('open');
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    const images = document.querySelectorAll(".slideshow img");
-    let currentSlide = 0;
-
-    function showSlide(index) {
-        images[currentSlide].classList.remove("active");
-        currentSlide = index;
-        images[currentSlide].classList.add("active");
-    }
-
-    function nextSlide() {
-        const nextIndex = (currentSlide + 1) % images.length;
-        showSlide(nextIndex);
-    }
-
-    function prevSlide() {
-        const prevIndex = (currentSlide - 1 + images.length) % images.length;
-        showSlide(prevIndex);
-    }
-
-    setInterval(nextSlide, 5000); // Change image every 3 seconds
-});
